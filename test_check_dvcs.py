eebf5cd36bf1069a6ca5e531ec83e90f8a96536f
jserafin <jserafin@redhat.com>
2024-11-25 17:56:30 -0300

    add unit tests

--- test_check_dvcs.py
+++ test_check_dvcs.py
@@ -4,6 +4,7 @@ import unittest
 from check_dvcs import does_string_start_with_jira
 from check_dvcs import delete_previous_comment_if_needed
 from check_dvcs import get_commit_jira_numbers
+from check_dvcs import _NO_JIRA_MARKER
 
 class TestDeletePreviousComment(unittest.TestCase):
 
@@ -16,14 +17,18 @@ class TestDeletePreviousComment(unittest.TestCase):
 class TestStringStartWithJira(unittest.TestCase):
 
     def test_string_start_with_jira_ticket(self):
-        self.assertEqual(does_string_start_with_jira("JIRA-1234 This is a PR"), "JIRA-1234")
+        self.assertEqual(does_string_start_with_jira("AAP-1234 This is a PR"), "AAP-1234")
 
     def test_string_with_no_jira_ticket(self):
-        self.assertEqual(does_string_start_with_jira("NO-JIRA: Minor changes"), "NO-JIRA")
+        self.assertEqual(does_string_start_with_jira(f"{_NO_JIRA_MARKER}: Minor changes"), _NO_JIRA_MARKER)
 
     def test_empty_string(self):
         """Test with an empty string."""
         self.assertIsNone(does_string_start_with_jira(""), "The PR string came back as None")
+
+    def test_random_string(self):
+        """Test with an random string."""
+        self.assertIsNone(does_string_start_with_jira("999990 - This is a PR"), "This is some PR title that does not match")
 
 class TestCommitJiraNumber(unittest.TestCase):
 
